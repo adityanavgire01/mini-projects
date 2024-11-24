437cb8bc4823fc0ccdcc7f00714300cf15c166d9
Aditya <aditya.p.navgire@gmail.com>
2024-11-24 11:44:01 +0530

    task-manager-app

---
+++ python-freecodeacademy/madlibs.py
@@ -0,0 +1,12 @@
+# string concatenation 
+
+character = input("Favorite Character: ")
+quote = input("Famous Quote from the Character: ")
+food = input("Favorite Food: ")
+place = input("Place they hang out: ")
+activity = input("Activity they enjoy: ")
+rival = input("Their Rival's Name: ")
+
+madlibs = f"{character} often says '{quote}'. They love eating {food} at {place}. When not at work, {character} enjoys {activity}, especially if it annoys {rival}!"
+
+print(madlibs)
